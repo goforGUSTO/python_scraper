f16eca7db7758626d48bed043309fdc90bfa3482
goforGUSTO <jamesdoth@gmail.com>
2020-10-06 16:52:51 -0700

    added destination/files(json,csv) for extracted data

--- demo_project/settings.py
+++ demo_project/settings.py
@@ -86,3 +86,4 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+FEED_URI = 'file:///C:\\Users\\james\\Desktop\\Misc\\data.json'
